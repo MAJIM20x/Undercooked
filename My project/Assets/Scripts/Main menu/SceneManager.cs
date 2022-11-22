8666b00ddcc036c375c9b3a13e97bfdd83f707c5
PanchoUnU <Francisco.concha01@alumnos.ucn.cl>
2022-11-21 22:51:23 -0300

    v0.10.3
    +Menu Settings
    +back main menu
    
    Co-Authored-By: MalikOwO <115024018+MalikOwO@users.noreply.github.com>
    Co-Authored-By: MAJIM20x <115025133+MAJIM20x@users.noreply.github.com>

--- My project/Assets/Scripts/Main menu/SceneManager.cs
+++ My project/Assets/Scripts/Main menu/SceneManager.cs
@@ -13,4 +13,8 @@ public class Scensemangar : MonoBehaviour
   {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
   }
+  public void MainMenuSettings()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+  }
 }

745a5e3d7d5f384c66815a7cc644eb31f30cfd31
PanchoUnU <Francisco.concha01@alumnos.ucn.cl>
2022-11-21 22:36:44 -0300

    v0.10.12
    +Menu control
    +Button back go
    
    Co-Authored-By: MalikOwO <115024018+MalikOwO@users.noreply.github.com>
    Co-Authored-By: MAJIM20x <115025133+MAJIM20x@users.noreply.github.com>

---
+++ My project/Assets/Scripts/Main menu/SceneManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scensemangar : MonoBehaviour
+{
+  public void MenuControl()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+  public void MainMenuControl()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+  }
+}

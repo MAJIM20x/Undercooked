34fc88597bbda326544bac31766dac9b5b2ab1c0
PanchoUnU <Francisco.concha01@alumnos.ucn.cl>
2022-11-28 23:48:37 -0300

    v0.11.6
    Cambios menu niveles

--- My project/Assets/Scripts/SceneMenu.cs
+++ My project/Assets/Scripts/SceneMenu.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
 public class SceneMenu : MonoBehaviour
 {
     [SerializeField] private bool isInside;
     public Rigidbody2D rb;
-    
+    public Collider2D box;
+    public GameObject Player;   
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Choque");
+            Debug.Log("Choque1");
             isInside = true;
         }
     }
@@ -18,6 +21,7 @@ public class SceneMenu : MonoBehaviour
     private void OnTriggerExit2D(Collider2D other) {
         if(other.gameObject.CompareTag("Player"))
         {
+
             Debug.Log("Choque");
             isInside = false;
         }
@@ -26,7 +30,16 @@ public class SceneMenu : MonoBehaviour
     private void Update() {
         if(Input.GetKeyDown(KeyCode.F) && isInside)
         {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("posicionX",(int)this.transform.position.x);
+            PlayerPrefs.SetInt("posicionY",(int)this.transform.position.y);
             Debug.Log("Cambio escena");
         }
     }
+    public void loadPosicion()
+    {
+        Player.transform.position = new Vector2( PlayerPrefs.GetFloat("X"),PlayerPrefs.GetFloat("Y"));
+        
+    }
+    
 }
